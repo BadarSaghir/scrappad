748ce6112b355d42131118713842d37e37c49b77
Sardar Badar <dataentrybadar@gmail.com>
2023-04-24 20:25:01 +0500

    added

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -1,3 +1,9 @@
+const fs = require("fs");
+const draw=require("./js/draw")
+const {createCanvas}=require('canvas')
+const canvas= createCanvas(400,400)
+const ctx=canvas.getContext("2d")
+
 const constants = {
   DATA_DIR: "../data",
   RAW_DIR: "",
@@ -9,11 +15,16 @@ const constants = {
 
 constants.RAW_DIR = constants.DATA_DIR + "/raw";
 constants.DATASET_DIR = constants.DATA_DIR + "/dataset";
+if (!fs.existsSync(constants.DATASET_DIR)) 
+fs.mkdirSync(constants.DATASET_DIR);
 constants.JSON_DIR = constants.DATASET_DIR + "/json";
+if (!fs.existsSync(constants.JSON_DIR)) 
+fs.mkdirSync(constants.JSON_DIR);
 constants.IMG_DIR = constants.DATASET_DIR + "/img";
+if (!fs.existsSync(constants.DATASET_DIR)) 
+fs.mkdirSync(constants.IMG_DIR);
 constants.SAMPLES = constants.DATASET_DIR + "/samples.json";
 
-const fs = require("fs");
 const fileName = fs.readdirSync(constants.RAW_DIR);
 /**
  * @type {{id:number,label:string,student_name:string,student_id:string}[]}
@@ -33,10 +44,26 @@ fileName.forEach((file) => {
       student_id: session,
       student_name: student,
     });
-    fs.writeFileSync(constants.JSON_DIR+"/"+id+".json", JSON.stringify(samples));
-    JSON.stringify(drawings[label])
+  const  paths= drawings[label]
+    fs.writeFileSync(
+      constants.JSON_DIR + "/" + id + ".json",
+      JSON.stringify(paths)
+    );
+    genrateImageFile(constants.JSON_DIR + "/" + id + ".png",paths)
+
     id++;
   }
 });
 
 fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
+
+/**
+ * @param {string} image
+ * @param {number[][][]} paths
+ */
+function genrateImageFile(image, paths) {
+  draw.paths(ctx,paths,"black")
+  const buffer=canvas.toBuffer("image/png")
+  fs.writeFileSync(image,buffer)
+}
+
---
+++ node/js/draw.js
@@ -0,0 +1,39 @@
+/**
+ * @global
+ */
+const draw = {
+  /**
+   *
+   * @param {CanvasRenderingContext2D | null} ctx
+   * @param {(number[])[]} path
+   * @param {string | CanvasGradient | CanvasPattern} color
+   */
+  path: (ctx, path, color = "black") => {
+    if (ctx) {
+      ctx.strokeStyle = color;
+      ctx.lineWidth = 3;
+      ctx.beginPath();
+      ctx.moveTo(path[0][0], path[0][1]);
+      path.forEach((cord, idx) => {
+        ctx.lineTo(cord[0], cord[1]);
+      });
+      ctx.lineCap = "round";
+      ctx.lineJoin = "round";
+
+      ctx.stroke();
+    }
+  },
+  /**
+   *
+   * @param {CanvasRenderingContext2D | null| any} ctx
+   * @param {(number[])[][]} paths
+   * @param {string | CanvasGradient | CanvasPattern} color
+   */
+  paths: (ctx, paths, color = "black") => {
+    paths.forEach((path, idx) => {
+      draw.path(ctx, path, color);
+    });
+  },
+};
+
+module.exports = draw;
