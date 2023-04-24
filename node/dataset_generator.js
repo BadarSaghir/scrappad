59ae99932b4332ff599bb886f753df85765f6079
Sardar Badar <dataentrybadar@gmail.com>
2023-04-24 18:51:45 +0500

    added folder

---
+++ node/dataset_generator.js
@@ -0,0 +1,15 @@
+const constants={
+    DATA_DIR:"../data",
+    RAW_DIR:"",
+    DATASET_DIR:"",
+    JSON_DIR:"",
+    IMG_DIR:"",
+    SAMPLES:""
+}
+
+constants.RAW_DIR=constants.DATA_DIR+"/raw"
+constants.DATASET_DIR=constants.DATA_DIR+"/dataset"
+constants.JSON_DIR=constants.DATASET_DIR+"/json"
+constants.IMG_DIR=constants.DATASET_DIR+"/img"
+constants.SAMPLES=constants.DATASET_DIR+"/samples.json"
+
